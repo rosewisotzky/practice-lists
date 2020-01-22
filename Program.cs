e66d9bd254690c73608d98e0118a38c065a3cfdc
Rose Wisotzky <rose@rosewisotzky.com>
2020-01-22 09:33:03 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace lists_exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
